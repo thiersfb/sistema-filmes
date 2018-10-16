a4fe7ac0ea519c49f80d197df88621b0fdba65f9
Thiers Barizon <thiers.fb@gmail.com>
2018-10-16 07:39:54 -0300

    Primeiro commit projeto de sistema de filmes em java

---
+++ src/main/java/web/ServletTeste.java
@@ -0,0 +1,19 @@
+package web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ServletTeste") //mapeamento de URL do Servlet; disponível a partir do servlet 3.1
+public class ServletTeste extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
